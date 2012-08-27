a776bc9f16e5f13f16b0b2b2c29dccc6ea934f2f
Sun Hao <wshadowhunter@yahoo.cn>
2012-08-27 14:40:29 -0400

    reviseF

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ if(isset($_POST['login'])){
 	session_start();
 	$username=trim($_POST['username']);
 	$password=sha1($username.$_POST['password']);
-	$redirect='http://login.orchestra.io/congrat.php';
+	$redirect='http://localhost:8888/SimpleLogin/congrat.php';
 	require_once('./authenticate.inc.php');
 	}
+
 ?>
 <!doctype html>
 <html>
@@ -17,7 +18,10 @@ if(isset($_POST['login'])){
 <script src="_js/jquery-1.6.3.min.js"></script>
 <script>
 $(document).ready(function(e) {
-	$('#form1').toggleClass('hide');
+	<?php if(isset($error)){?>
+	$('#form1').show();
+	$('#fadeto').hide();
+	<?php }?>
     $('#fadeto').fadeOut(2000, function(){
 		$('#form1').show();
 		}
@@ -30,7 +34,10 @@ $(document).ready(function(e) {
 <?php
 if($error) echo "<p>$error</p>";
 ?>
+
 <h2 id="fadeto">Welcome to TimeGrab!</h2>
+
+<?php if((isset($error) && $_POST) || !$_POST) {?>
 <form id="form1" name="form1" method="post" action="">
 <p>
   <label for="username">Username:</label>
@@ -44,5 +51,6 @@ if($error) echo "<p>$error</p>";
 <input name="login" type="submit" value="Log in" />
 </p>
 </form>
+<?php } ?>
 </body>
 </html>
